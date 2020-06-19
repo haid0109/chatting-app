50e9670042209b149b3f2f7dd98cdd0cb88e7048
haid0109 <51113567+haid0109@users.noreply.github.com>
2020-06-19 10:40:59 +0200

    created table users

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 import psycopg2
 
-con = psycopg2.connect(
-    host = 'localhost',
-    port = 8624,
-    database = 'chattingApp',
-    user = 'haidar',
-    password = 'Passw0rd'
-)
+try:
+    con = psycopg2.connect(
+        host = 'localhost',
+        port = 8624,
+        database = 'chattingApp',
+        user = 'haidar',
+        password = 'Passw0rd'
+    )
+    cur = con.cursor()
+except Exception as error:
+    print(f'could not connect to database: {error}')
 
+try:
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(50) UNIQUE NOT NULL,
+        password VARCHAR(50) NOT NULL
+        );'''
+    )
+except Exception as error:
+    con.rollback()
+    print(f'could not create table users: {error}')
 
-
+cur.close()
 con.close()
